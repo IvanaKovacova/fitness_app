ce746029636ed12d93c32f39418b017a70da96d2
Kovacova <Ivana.Kovacova@dell.com>
2022-12-07 18:24:11 +0100

    message if no data

--- main.py
+++ main.py
@@ -35,55 +35,59 @@ with st.sidebar:
     strava_widget = "<h3><iframe allowtransparency frameborder='0' height='300' scrolling='yes' src='https://www.strava.com/clubs/995662/latest-rides/84e66ad0283c10bf7ace6e5b31be4058becca9c3?show_rides=false' width='285'></iframe></h3>"
     st.markdown(strava_widget, unsafe_allow_html=True)
 
-
-tab1, tab2, tab3, tab4 = st.tabs(['Team Standings','Activities Overview', 'Milestones Achievement', 'General Information'])
-
-with tab1:  
-    team_graph()
+df = pd.read_excel('data/data_all.xlsx', usecols = ['Name', 'Activity', 'Date'])
+if len(df) < 2:
+    st.header('There are no data to display yet')
+else:
     
-with tab2:
-    overview()
+    tab1, tab2, tab3, tab4 = st.tabs(['Team Standings','Activities Overview', 'Milestones Achievement', 'General Information'])
     
-   
-with tab3:
-    milestone()
-    
-with tab4:
-    st.subheader('Links')
-    url='https://www.strava.com/clubs/DCFC'
-    st.write("[Strava Club](%s)" % url)
-    
-    
-    st.subheader('Downloads')
-
-    with open("data/rulebook.pdf", "rb") as pdf_file:
-        pdf = pdf_file.read()
+    with tab1:  
+        team_graph()
         
-    st.download_button(label="Download Rulebook",
-                       data=pdf,
-                       file_name="Rulebook.pdf",
-                       mime='application/octet-stream')
+    with tab2:
+        overview()
         
-    with open("data/Fitness Challenge.pdf", "rb") as pdf_file:
-        pdf = pdf_file.read()
-
-    st.download_button(label="Download Points Calculation Overview",
-                       data=pdf,
-                       file_name="Fitness Challenge.pdf",
-                       mime='application/octet-stream')
+       
+    with tab3:
+        milestone()
+        
+    with tab4:
+        st.subheader('Links')
+        url='https://www.strava.com/clubs/DCFC'
+        st.write("[Strava Club](%s)" % url)
+        
+        
+        st.subheader('Downloads')
     
-    st.subheader('Contacts')
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        st.markdown('***If you have any questions contact:***')
-        st.write('Subramita Dash - subramita_dash@dell.com')
-        st.write('Madhav Parashar - madhav_parashar@dell.com')
-        st.write('Sanjay Kumar - sanjay_kumar29@dell.com')
-        st.write('Ivana Kovacova - ivana.kovacova@dell.com')
-    with col2:
-        st.markdown('***For issues with the app:***')
-        st.write('Ivana Kovacova - ivana.kovacova@dell.com')
+        with open("data/rulebook.pdf", "rb") as pdf_file:
+            pdf = pdf_file.read()
+            
+        st.download_button(label="Download Rulebook",
+                           data=pdf,
+                           file_name="Rulebook.pdf",
+                           mime='application/octet-stream')
+            
+        with open("data/Fitness Challenge.pdf", "rb") as pdf_file:
+            pdf = pdf_file.read()
+    
+        st.download_button(label="Download Points Calculation Overview",
+                           data=pdf,
+                           file_name="Fitness Challenge.pdf",
+                           mime='application/octet-stream')
+        
+        st.subheader('Contacts')
+        col1, col2, col3 = st.columns(3)
+    
+        with col1:
+            st.markdown('***If you have any questions contact:***')
+            st.write('Subramita Dash - subramita_dash@dell.com')
+            st.write('Madhav Parashar - madhav_parashar@dell.com')
+            st.write('Sanjay Kumar - sanjay_kumar29@dell.com')
+            st.write('Ivana Kovacova - ivana.kovacova@dell.com')
+        with col2:
+            st.markdown('***For issues with the app:***')
+            st.write('Ivana Kovacova - ivana.kovacova@dell.com')
        
 
 col1, col2, col3 = st.columns(3)
