42a196f7fd12bcbbcf3586813284df20dfaae6b9
Iyva <kotlarova.ivana@gmail.com>
2023-01-02 10:10:02 +0100

    redoing loading new data

--- data_update.py
+++ data_update.py
@@ -7,8 +7,8 @@ df = pd.read_excel('data/data_all.xlsx', dtype='object')
 df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
 
 # Import new data
-# df_new = pd.read_csv('data/data_new.csv', dtype='object', engine = 'python')  
-df_new = pd.read_csv('data/data_new.csv', dtype='object', engine = 'python', sep=';')  
+df_new = pd.read_csv('data/data_new.csv', dtype='object', engine = 'python')  
+# df_new = pd.read_csv('data/data_new.csv', dtype='object', engine = 'python', sep=';')  
 
 df_new.rename(columns= {list(df_new)[1]: 'Activity_ID', list(df_new)[2]: 'Activity'}, inplace = True)
 
